ff1ca5e271f997f619b3f5d181ecff56082461da
ElsyCuevasDLT <a23310379@ceti.mx>
2026-09-23 01:32:06 +0000

    epuck rueda 2.0

--- controllers/epuck_rueda/epuck_rueda.cpp
+++ controllers/epuck_rueda/epuck_rueda.cpp
@@ -13,12 +13,11 @@ int main() {
   if (left_wheel == nullptr)
     return 1;
 
-  // Una vuelta completa de una sola llanta del e-puck.
   constexpr double full_turn = 2.0 * 3.14159265358979323846;
   left_wheel->setPosition(full_turn);
 
   while (robot.step(time_step) != -1) {
-    // El controlador de posición interno del motor realiza el movimiento.
+
   }
 
   return 0;
